acd06648fb9c00fa3527ca15167f78b6954f8e31
nakashimashin <nakashimashin0517@gmail.com>
2024-06-08 17:57:48 +0900

    shadcn追加

--- src/app/Auth/page.tsx
+++ src/app/Auth/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
 
 function Auth() {
 	return (
 		<div>
 			<div>Auth</div>
+			<Button>Button</Button>
 		</div>
 	);
 }
