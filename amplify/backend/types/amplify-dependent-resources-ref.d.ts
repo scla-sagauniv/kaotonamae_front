5193d1d0bf9f4710f5cbe16ce009da1c8f9823cd
nakashimashin <nakashimashin0517@gmail.com>
2024-06-09 17:55:45 +0900

    Amplify Authカテゴリを追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+	auth: {
+		kaotonamae0e327acd: {
+			AppClientID: 'string';
+			AppClientIDWeb: 'string';
+			IdentityPoolId: 'string';
+			IdentityPoolName: 'string';
+			UserPoolArn: 'string';
+			UserPoolId: 'string';
+			UserPoolName: 'string';
+		};
+	};
+};
