631bf2ecf2db82af4fb607f70ac1f91665ece822
nakashimashin <nakashimashin0517@gmail.com>
2024-06-10 00:03:14 +0900

    サインアップ機能実装(パスコード送信まで)

---
+++ src/components/auth/ConfigureAmplifyClientSide.ts
@@ -0,0 +1,10 @@
+'use client';
+
+import { Amplify } from 'aws-amplify';
+import config from '../../aws-exports';
+
+Amplify.configure(config, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+	return null;
+}
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -6,6 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { signUpSchema } from '@/utils/validationSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { signUp } from 'aws-amplify/auth';
 
 function SignUp() {
 	const {
@@ -17,65 +18,79 @@ function SignUp() {
 		resolver: zodResolver(signUpSchema),
 	});
 
-	const onSubmit = (data: SignUpType) => {
-		console.log('Sign Up');
-		console.log(data);
+	const onSubmit = async (data: SignUpType) => {
+		if (data.password !== data.confirmPassword) {
+			console.error('Password does not match');
+			return;
+		}
+		try {
+			const userData = await signUp({
+				username: data.email,
+				password: data.password,
+				options: {
+					userAttributes: {
+						email: data.email,
+					},
+				},
+			});
+			console.log('Sign Up', userData);
+		} catch (error) {
+			console.error('Error during sign up:', error);
+		}
 	};
 
 	return (
-		<>
-			<Card>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<CardContent className="space-y-2 mt-3">
-						<div className="space-y-1">
-							<Label htmlFor="new_email">Email</Label>
-							<Input
-								id="new_email"
-								type="email"
-								placeholder="メールアドレス"
-								{...register('email')}
-							/>
-							{errors.email && (
-								<span className="text-red-500 text-[13px]">
-									{errors.email.message}
-								</span>
-							)}
-						</div>
-						<div className="space-y-1">
-							<Label htmlFor="new_password">Password</Label>
-							<Input
-								id="new_password"
-								type="password"
-								placeholder="パスワード"
-								{...register('password')}
-							/>
-							{errors.password && (
-								<span className="text-red-500 text-[13px]">
-									{errors.password.message}
-								</span>
-							)}
-						</div>
-						<div className="space-y-1">
-							<Label htmlFor="re_password">Password(再入力)</Label>
-							<Input
-								id="re_password"
-								type="password"
-								placeholder="パスワード"
-								{...register('confirmPassword')}
-							/>
-							{errors.confirmPassword && (
-								<span className="text-red-500 text-[13px]">
-									{errors.confirmPassword.message}
-								</span>
-							)}
-						</div>
-					</CardContent>
-					<CardFooter>
-						<Button className="w-full">Submit</Button>
-					</CardFooter>
-				</form>
-			</Card>
-		</>
+		<Card>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<CardContent className="space-y-2 mt-3">
+					<div className="space-y-1">
+						<Label htmlFor="new_email">Email</Label>
+						<Input
+							id="new_email"
+							type="email"
+							placeholder="メールアドレス"
+							{...register('email')}
+						/>
+						{errors.email && (
+							<span className="text-red-500 text-[13px]">
+								{errors.email.message}
+							</span>
+						)}
+					</div>
+					<div className="space-y-1">
+						<Label htmlFor="new_password">Password</Label>
+						<Input
+							id="new_password"
+							type="password"
+							placeholder="パスワード"
+							{...register('password')}
+						/>
+						{errors.password && (
+							<span className="text-red-500 text-[13px]">
+								{errors.password.message}
+							</span>
+						)}
+					</div>
+					<div className="space-y-1">
+						<Label htmlFor="re_password">Password(再入力)</Label>
+						<Input
+							id="re_password"
+							type="password"
+							placeholder="パスワード"
+							{...register('confirmPassword')}
+						/>
+						{errors.confirmPassword && (
+							<span className="text-red-500 text-[13px]">
+								{errors.confirmPassword.message}
+							</span>
+						)}
+					</div>
+				</CardContent>
+				<CardFooter>
+					<Button className="w-full">Submit</Button>
+				</CardFooter>
+			</form>
+		</Card>
 	);
 }
 
