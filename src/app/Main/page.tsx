28cd2dd5311a4df0bddbc10754bf6ed36ee85d7f
nakashimashin <nakashimashin0517@gmail.com>
2024-06-11 00:42:35 +0900

    認証済みユーザーを取得することができる

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -2,9 +2,21 @@
 
 import { Button } from '@/components/ui/button';
 import { signOut } from 'aws-amplify/auth';
+import { getCurrentUser } from 'aws-amplify/auth';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 function Main() {
+	useEffect(() => {
+		const fetchUser = async () => {
+			const { userId, username, signInDetails } = await getCurrentUser();
+			console.log('user id: ', userId);
+			console.log('username: ', username);
+			console.log('sign-in details: ', signInDetails);
+		};
+		fetchUser();
+	}, []);
+
 	const router = useRouter();
 	const handleSignOut = async () => {
 		try {
