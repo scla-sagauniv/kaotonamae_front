f642a829b703602ba2c6b4f608ec78fd0c1b88e3
t_takumaru <t.takuma.saga.u@gmail.com>
2024-06-16 10:21:55 +0900

    Groupの画面遷移は実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ import axios from 'axios';
 import { useOneTimePassStore } from '@/store/oneTimePassStore';
 import Header from '@/components/Header';
 import { GroupType } from '@/types/index';
+import Link from 'next/link'; // next/linkをインポート
 
 export default function Home() {
 	const { isOneTimePass, setOneTimePassFalse } = useOneTimePassStore();
-	const [objects, setObjects] = useState<GroupType[]>([]); // 初期値を空配列に設定し、型を適用
-	const [error, setError] = useState<string | null>(null); // エラーメッセージ用の状態
+	const [objects, setObjects] = useState<GroupType[]>([]);
+	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
 		const fetchUserAndGroups = async () => {
@@ -23,21 +24,19 @@ export default function Home() {
 				console.log('username: ', username);
 				console.log('sign-in details: ', signInDetails);
 
-				console.log(
-					`${process.env.NEXT_PUBLIC_VITE_GO_APP_API_URL}/groups/${userId}`,
-				);
-
 				const res = await axios.get<GroupType[]>(
 					`${process.env.NEXT_PUBLIC_VITE_GO_APP_API_URL}/groups/${userId}`,
 				);
 
 				if (res.data && res.data.length > 0) {
-					setObjects(res.data); // 取得したデータで状態を更新
+					setObjects(res.data);
 				} else {
-					setObjects([]); // 空のレスポンスの場合は空配列を設定
+					setObjects([]);
+					setError('グループが見つかりませんでした。');
 				}
 			} catch (error) {
 				console.error('Error fetching user or groups:', error);
+				setError('データを取得中にエラーが発生しました。');
 			}
 		};
 
@@ -55,13 +54,15 @@ export default function Home() {
 						</div>
 					) : objects.length > 0 ? (
 						objects.map((object) => (
-							<div
+							<Link
 								key={object.groupId}
+								href={`/Group/${object.groupId}`} // href属性にページのパスを設定
+								passHref // リンク内の要素にhrefを渡す
 								className="flex flex-row items-center border border-black h-[50px] space-x-2 w-full p-3"
 							>
 								<div className="rounded-full bg-gray-200 w-10 h-10 ml-2"></div>
 								<p>{object.groupName}</p>
-							</div>
+							</Link>
 						))
 					) : (
 						<div className="flex justify-center items-center h-[50px] w-full p-3">
