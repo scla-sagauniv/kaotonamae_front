d2cb0c79788f08af22af238ab913f93d48aabc48
GeN403 <hajimeh0410@icloud.com>
2024-06-16 11:34:36 +0900

    正解不正解の画面ができたのでコミットします

--- src/app/QuizCorrect/page.tsx
+++ src/app/QuizCorrect/page.tsx
@@ -12,7 +12,7 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-const QuizCorrect = () => {
+function QuizCorrect() {
 	const router = useRouter();
 	// ページ遷移先の設定まだです
 	const end = () => {
@@ -22,25 +22,35 @@ const QuizCorrect = () => {
 		router.push('/');
 	};
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1>正解です。</h1>
-			<div className="rounded-full bg-gray-200 w-[150px] h-[150px]"></div>
-			<h1>素晴らしい</h1>
-			<div>
-				<button
-					onClick={end}
-					className="inline-flex h-12 items-center justify-center rounded-md bg-green-600 px-6 font-medium text-neutral-50 transition active:scale-110 "
-				>
-					終了
-				</button>
-				<button
-					onClick={next}
-					className="inline-flex h-12 items-center justify-center rounded-md bg-blue-600 px-6 font-medium text-neutral-50 transition active:scale-110"
-				>
-					次へ
-				</button>
-			</div>
+		<div className="flex min-h-screen justify-center items-center px-10">
+			<Card className="items-center">
+				<CardHeader className="flex justify-center w-full">
+					<CardTitle className="text-center">正解です。</CardTitle>
+					<CardDescription className="text-center">
+						<CardContent className="rounded-full bg-gray-200 w-[150px] h-[150px]"></CardContent>
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="flex justify-center w-full">
+					<p className="text-center">素晴らしい</p>
+				</CardContent>
+				<CardFooter className="flex justify-center w-full">
+					<div className="flex space-x-4">
+						<button
+							onClick={end}
+							className="inline-flex h-12 items-center justify-center rounded-md bg-green-600 px-6 font-medium text-neutral-50 transition active:scale-110 "
+						>
+							終了
+						</button>
+						<button
+							onClick={next}
+							className="inline-flex h-12 items-center justify-center rounded-md bg-blue-600 px-6 font-medium text-neutral-50 transition active:scale-110"
+						>
+							次へ
+						</button>
+					</div>
+				</CardFooter>
+			</Card>
 		</div>
 	);
-};
+}
 export default QuizCorrect;
--- src/app/QuizFalse/page.tsx
+++ src/app/QuizFalse/page.tsx
@@ -2,7 +2,15 @@
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-const QuizFalse = () => {
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+function QuizFalse() {
 	const router = useRouter();
 	// ページ遷移先の設定まだです
 	const end = () => {
@@ -12,25 +20,35 @@ const QuizFalse = () => {
 		router.push('/');
 	};
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1>正解は</h1>
-			<div className="rounded-full bg-gray-200 w-[150px] h-[150px]"></div>
-			<h1>Aさんです</h1>
-			<div>
-				<button
-					onClick={end}
-					className="inline-flex h-12 items-center justify-center rounded-md bg-green-600 px-6 font-medium text-neutral-50 transition active:scale-110 "
-				>
-					終了
-				</button>
-				<button
-					onClick={next}
-					className="inline-flex h-12 items-center justify-center rounded-md bg-blue-950 px-6 font-medium text-neutral-50 transition active:scale-110 "
-				>
-					次へ
-				</button>
-			</div>
+		<div className="flex min-h-screen justify-center items-center px-10">
+			<Card className="items-center">
+				<CardHeader className="flex justify-center w-full">
+					<CardTitle className="text-center">正解は</CardTitle>
+					<CardDescription className="text-center">
+						<CardContent className="rounded-full bg-gray-200 w-[150px] h-[150px]"></CardContent>
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="flex justify-center w-full">
+					<p className="text-center">Aさんです</p>
+				</CardContent>
+				<CardFooter className="flex justify-center w-full">
+					<div className="flex space-x-4">
+						<button
+							onClick={end}
+							className="inline-flex h-12 items-center justify-center rounded-md bg-green-600 px-6 font-medium text-neutral-50 transition active:scale-110 "
+						>
+							終了
+						</button>
+						<button
+							onClick={next}
+							className="inline-flex h-12 items-center justify-center rounded-md bg-blue-600 px-6 font-medium text-neutral-50 transition active:scale-110"
+						>
+							次へ
+						</button>
+					</div>
+				</CardFooter>
+			</Card>
 		</div>
 	);
-};
+}
 export default QuizFalse;
