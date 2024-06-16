97e49beebbcf6ef164b7a6f486dff3872f0c6c18
t_takumaru <t.takuma.saga.u@gmail.com>
2024-06-16 13:39:40 +0900

    Groupmemberを取得して表示できるようになった

--- src/app/Group/[id]/page.tsx
+++ src/app/Group/[id]/page.tsx
@@ -1,5 +1,3 @@
-// pages/Group/[id]/page.tsx
-
 'use client';
 
 import Header from '@/components/Header';
@@ -7,46 +5,39 @@ import { Button } from '@/components/ui/button';
 import { useRouter, useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { GroupType } from '@/types/index';
-
-// const objects = [
-// 	{ name: 'ユーザー1' },
-// 	{ name: 'ユーザー2' },
-// 	{ name: 'ユーザー3' },
-// 	{ name: 'ユーザー4' },
-// 	{ name: 'ユーザー5' },
-// 	{ name: 'ユーザー6' },
-// 	{ name: 'ユーザー7' },
-// 	{ name: 'ユーザー8' },
-// 	{ name: 'ユーザー9' },
-// 	{ name: 'ユーザー10' },
-// ];
+import { GroupType, GroupMemberType } from '@/types/index';
 
 function GroupPage() {
 	const router = useRouter();
 	const Params = useParams();
 	const id = Params.id; // ここで動的パラメータを取得
-	const [groupMembers, setGroupMembers] = useState<GroupType[]>([]);
+	const [groupMembers, setGroupMembers] = useState<GroupMemberType[]>([]);
 	const [groupName, setGroupName] = useState('');
 	const [overview, setOverview] = useState('');
 
 	useEffect(() => {
 		const fetchGroupsAndGroupMembers = async () => {
 			try {
-				const res = await axios.get<GroupType>(
+				const Groups = await axios.get<GroupType>(
 					`${process.env.NEXT_PUBLIC_VITE_GO_APP_API_URL}/group/${id}`,
 				);
-				const groupData = res.data; // groupData の型は GroupType
+				const groupData = Groups.data; // groupData の型は GroupType
 				// データをセットする例（適宜 useState を使用する前提で記述）
 				setGroupName(groupData.groupName);
 				setOverview(groupData.overview);
+				const GroupMembers = await axios.get<GroupMemberType[]>(
+					`${process.env.NEXT_PUBLIC_VITE_GO_APP_API_URL}/groupMembers/${id}`,
+				);
+				const groupMemberData = GroupMembers.data;
+				setGroupMembers(groupMemberData);
+				console.log(groupMembers);
 			} catch (error) {
 				console.error('Error fetching groups:', error);
 			}
 		};
 
 		fetchGroupsAndGroupMembers();
-	}, []);
+	}, [id]); // idを依存配列に追加
 
 	return (
 		<div className="h-screen w-screen">
@@ -61,15 +52,15 @@ function GroupPage() {
 					{overview}
 				</div>
 				<div className="flex flex-col mt-[25px] overflow-y-auto max-h-[250px] w-10/12">
-					{/* {objects.map((object, index) => (
+					{groupMembers.map((member, index) => (
 						<div
 							key={index}
 							className="flex flex-row items-center border border-black h-[50px] space-x-2 w-full p-3"
 						>
 							<div className="rounded-full bg-gray-200 w-10 h-10 ml-2"></div>
-							<p>{}</p>
+							<p>{member.memberName}</p>
 						</div>
-					))} */}
+					))}
 				</div>
 				<Button
 					onClick={() => {
