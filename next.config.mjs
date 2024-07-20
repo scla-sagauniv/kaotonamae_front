330d9c8fdfb7145601f76bac6808d3bb9070bfa2
nakashimashin <nakashimashin0517@gmail.com>
2024-07-20 17:28:52 +0900

    next/imageで外部ドメインを使用可能にする

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ['kaotonamae.s3.ap-northeast-1.amazonaws.com'],
+	},
+};
 
 export default nextConfig;
