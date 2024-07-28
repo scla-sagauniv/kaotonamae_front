57583e1202b9a0e356002770152fde5b61270dc7
nakashimashin <nakashimashin0517@gmail.com>
2024-07-29 00:03:01 +0900

    友達の情報を表示

--- src/app/FriendPage/[userId]/page.tsx
+++ src/app/FriendPage/[userId]/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+
+import Header from '@/components/Header';
+import DisPlayUserInfo from '@/components/userInfo/DisPlayUserInfo';
+
 function FriendPage() {
-	return <div>友だち</div>;
+	return (
+		<div>
+			<Header />
+			<div className="flex flex-col mt-[75px]">
+				<DisPlayUserInfo />
+			</div>
+		</div>
+	);
 }
 
 export default FriendPage;
